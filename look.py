345223d800e2b4deee049412878401e352796c58
4625204 <a4625204@gmail.com>
2019-01-04 00:50:24 +0800

    lint and optimize code

--- look.py
+++ look.py
@@ -1,37 +1,23 @@
-def look(Input):
+sign = lambda x: [1, -1][x < 0]
+
+def look(Input,args):
     passenger1 = []
     passenger2 = []
+    passenger3 = []
 
-    if direction == 1:
-        for person in Input:
-            if person['direction'] == 1 :
-                if person['enter_floor'] > current_floor : 
-                    passenger1.append(person)
-                    Input.remove(person)
-                else:
-                    passenger2.append(person)
-                    Input.remove(person)
-        passenger1=sorted(passenger1, key = lambda x : x['exit_floor']) 
-        passenger2=sorted(passenger2, key = lambda x : x['exit_floor']) 
-        Input=sorted(Input, key = lambda x : x['exit_floor'],reverse=True)
+    for person in Input:
+        if person['direction'] == args['direction'] :
+            if sign(person[args['target']] - args['current_floor']) == args['direction']: 
+                passenger1.append(person)
+                Input.remove(person)
+            else:
+                passenger3.append(person)
+                Input.remove(person)
+
+    passenger1=sorted(passenger1, key = lambda x : x[args['target']], reverse = (args['direction'] == -1)) 
+    passenger3=sorted(passenger3, key = lambda x : x[args['target']], reverse = (args['direction'] == -1)) 
+    passenger2=sorted(Input, key = lambda x : x[args['target']],reverse = (args['direction'] == 1))
         
-    else:
-        for person in Input:
-            if person['direction'] == -1 :
-                if person['enter_floor'] < current_floor : 
-                    passenger1.append(person)
-                    Input.remove(person)
-                else :
-                    passenger2.append(person)
-                    Input.remove(person)
-            
-        passenger1 = sorted(passenger1, key = lambda x : x['exit_floor'],reverse=True) 
-        passenger2 = sorted(passenger2, key = lambda x : x['exit_floor'],reverse=True) 
-        Input = sorted(Input, key = lambda x : x['exit_floor'])
-    Output = passenger1 + Input + passenger2
+    Output = passenger1 + passenger2 + passenger3 
     
-    return Output.copy()
-
-
-
-        
+    return Output.copy()       
--- scan_edf.py
+++ scan_edf.py
@@ -1,8 +1,7 @@
 
-def scan_edf(Input):
+def scan_edf(Input,args):
     new_Input = []
     current_min = 9999
-    current_floor = 1
     Output=[]
     request=0
 
@@ -18,11 +17,10 @@ def scan_edf(Input):
             if person['time_limit'] < current_min :  #尋找最小的time limit
                 current_min = person['time_limit']
                 request = person['id']
-                diff = abs(person['enter_floor'] - current_floor)
+                diff = abs(person['enter_floor'] - args['current_floor'])
             elif (person['time_limit'] == current_min): #一樣的time limit就scan，看哪層離電梯比較近
-                if (diff > abs(person['enter_floor'] - current_floor)):
+                if (diff > abs(person['enter_floor'] - args['current_floor'])):
                     request = person['id']
-                    diff = abs(person['enter_floor'] - current_floor)
                                                                                       
         current_min = 9999
         for person in new_Input:
