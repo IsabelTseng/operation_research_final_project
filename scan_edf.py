9211ffd75cab8679a4e83e036ccabec1ed3c85c1
4625204 <a4625204@gmail.com>
2019-01-03 16:49:54 +0800

    version2

--- scan_edf.py
+++ scan_edf.py
@@ -1,25 +1,31 @@
 import random
 Input = []
-for i in range(1,20):
+for i in range(1,100):
     person = {}
     person['id'] = i
     person['enter_floor'] = random.randint(1,12)      
     person['exit_floor'] = random.randint(1,12)
-    person['time_quantum'] = random.randint(30,60)
-    person['intime'] = random.randint(1,61)
+    person['time_quantum'] = random.randint(60,200)
+    person['intime'] = random.randint(1,3600)
     Input.append(person)
 
 def scan_edf(Input):
     new_Input=[]
+    pesenger=[]
     current_min=9999
     current_floor=1
     current_time=0
-    current_task=1
+    current_task=0
     direction=0
     waiting_time=0
     diff=0
     bias=0
+    max_pesenger=15
     Output=[]
+    temp_enter=0
+    temp_exit=0
+    enter_time=0
+    exit_time=0
 
     for person in Input:
         newperson=person.copy()
@@ -34,26 +40,47 @@ def scan_edf(Input):
         for person in new_Input:
             if (person['time_limit']<current_min):  #尋找最小的time limit
                 current_min=person['time_limit']
-                current_task=person['id']
-                diff=abs(person['enter_floor']-current_floor)                
+                current_task=(person['id'])
+                diff=abs(person['enter_floor']-current_floor)
+                temp_enter=person['enter_floor']
+                temp_exit=person['exit_floor']
+                if(current_time<person['intime']):
+                    enter_time=person['intime']+diff*2+2
+                else:
+                    enter_time=current_time+diff*2+2
+                exit_time=enter_time+(abs(person['enter_floor']-person['exit_floor']))*2+2+15*2
             elif (person['time_limit']==current_min): #一樣的time limit就scan，看哪層離電梯比較近
                 if (diff>abs(person['enter_floor']-current_floor)):
                     current_task=person['id']
                     diff=abs(person['enter_floor']-current_floor)
+        
 
+
+        current_time=exit_time                                                                                                           
+        current_floor=temp_exit
+        current_min=9999
         for person in new_Input:
-            if(person['id']==current_task):
-                current_time=person['intime']+diff*2+(abs(person['exit_floor']-person['enter_floor']))*2+2*8  
-                waiting_time=current_time-person['intime']
-                bias=person['time_limit']-current_time
-                direction=person['direction']
-                current_min=9999
-                new_Input.remove(person)
+            if(person['id']==current_task or (person['intime']<enter_time and  person['enter_floor']==temp_enter and person['exit_floor']==temp_exit) ):
+            #if(person['id']==current_task):
+                if(len(pesenger)<max_pesenger):
+                    pesenger.append(person)
+                    new_Input.remove(person)
+        for person in pesenger:
+                
+                #if (current_time<person['intime']):
+                 #   current_time=person['intime']+diff*2
+                #else:
+                 #   current_time=current_time+diff*2
+                waiting_time=enter_time-person['intime']
+                #current_time=current_time+(abs(person['enter_floor']-person['exit_floor']))*2+8+8
+                bias=person['time_quantum']-waiting_time
                 in_person={}
                 in_person['id']=current_task
                 in_person['waiting_time']=waiting_time
                 in_person['bias']=bias
+                pesenger.remove(person)
                 Output.append(in_person)
+
     return Output.copy()
 
 print(*Input,sep='\n')
