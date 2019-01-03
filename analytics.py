717e24f849b1a2d3f535fb666091af755025192e
kaeteyaruyo <kaeteyaruyo@gmail.com>
2019-01-04 00:16:55 +0800

    Add image directory

--- analytics.py
+++ analytics.py
@@ -7,5 +7,5 @@ def get_histogram(data, title, xlabel, ylabel, filename):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.savefig(f"./image/{ filename }.png")
+    plt.savefig(f"image/{ filename }.png")
     plt.close()
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import input_generator as gen
-import algorithms.scan_edf
+#import algorithms.scan_edf
 import analytics as aly
 
 for i in range(10):
     input = gen.get_input()
-    count = [0 for i in range(3600 // 30)]
+    count = [0 for i in range(3600 // 5)]
     for x in input:
-        count[(x['intime'] // 30) - 1] += 1
+        count[(x['timestamp'] // 5) - 1] += 1
 
-    aly.get_histogram(count, title = 'request per minute', xlabel = 'request', ylabel = 'count', filename = f'{ i + 1}')
+    aly.get_histogram(count, title = 'request per 5 seconds', xlabel = 'request', ylabel = 'count', filename = f'{ i + 1 }')
