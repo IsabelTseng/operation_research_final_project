320b5791636bbb9867dce9b6f6aa61bd28093e5d
IsabelTseng <isabel870310@gmail.com>
2019-01-04 05:38:24 +0800

    Fix person disappear bug of LOOK

--- algorithms/look.py
+++ algorithms/look.py
@@ -5,19 +5,21 @@ def look(Input,args):
     passenger2 = []
     passenger3 = []
 
-    for person in Input:
+    newInput = Input.copy()
+    for i in range(len(newInput)):
+        person = newInput[len(Input)-i-1]
         if person['direction'] == args['direction'] :
             if sign(person[args['target']] - args['current_floor']) == args['direction']: 
                 passenger1.append(person)
-                Input.remove(person)
+                newInput.remove(person)
             else:
                 passenger3.append(person)
-                Input.remove(person)
+                newInput.remove(person)
 
     passenger1=sorted(passenger1, key = lambda x : x[args['target']], reverse = (args['direction'] == -1)) 
     passenger3=sorted(passenger3, key = lambda x : x[args['target']], reverse = (args['direction'] == -1)) 
-    passenger2=sorted(Input, key = lambda x : x[args['target']],reverse = (args['direction'] == 1))
-        
+    passenger2=sorted(newInput, key = lambda x : x[args['target']],reverse = (args['direction'] == 1))
+    
     Output = passenger1 + passenger2 + passenger3 
     
     return Output.copy()       
