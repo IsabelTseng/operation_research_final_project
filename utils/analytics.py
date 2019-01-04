6989606696c4c554a6288e2d28b888e747d7a633
kaeteyaruyo <kaeteyaruyo@gmail.com>
2019-01-04 09:26:40 +0800

    Add bias barh plot

--- utils/analytics.py
+++ utils/analytics.py
@@ -15,3 +15,13 @@ def get_histogram(data, title, xlabel, ylabel, filename, x_min, x_max, bins):
     plt.xlim(x_min, x_max)
     plt.savefig(f"image/{ filename }.png")
     plt.close()
+
+def get_multibarhplot(data1, data2, bin_tags, title):
+    index = np.arange(len(data1))
+    bin_width = 0.5
+    plt.title(title)
+    plt.barh(index, data1, bin_width, color = 'C0')
+    plt.barh(index + bin_width, data2, bin_width, color = 'C1')
+    plt.yticks(index + 1.5 * bin_width, bin_tags)
+    plt.savefig(f"image/{ title }.png")
+    plt.close()
